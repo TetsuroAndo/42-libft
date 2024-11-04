dfe365fac9e05769969f5ceef1f6cefcc1663994
TetsuroAndo <teando@c2r6s10.42tokyo.jp>
2024-11-04 22:21:54 +0900

    norming

--- src/ft_stdio/ft_putnbr.c
+++ src/ft_stdio/ft_putnbr.c
@@ -1,4 +1,4 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 21:23:32 by teando            #+#    #+#             */
-/*   Updated: 2024/11/04 21:34:44 by teando           ###   ########.fr       */
+/*   Updated: 2024/11/04 22:09:21 by teando           ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "ft_stdio.h"
 #include <stddef.h>
@@ -19,9 +19,9 @@ int	ft_putnbr(int n)
 	char			buffer[12];
 	unsigned int	num;
 	char			*c;
-  int count;
+	int				count;
 
-  num = n ^ ((n >> 31) & ((unsigned int)(-n - n)));
+	num = n ^ ((n >> 31) & ((unsigned int)(-n - n)));
 	c = &buffer[12];
 	*--c = '\0';
 	while (num || !*c)
@@ -31,12 +31,12 @@ int	ft_putnbr(int n)
 	}
 	if (n < 0)
 		*--c = '-';
-  count = 0;
-  while (*c)
-  {
-    if(ft_putchar(*c++) == EOF)
-      return (EOF);
-    count++;
-  }
-  return (count);
+	count = 0;
+	while (*c)
+	{
+		if (ft_putchar(*c++) == EOF)
+			return (EOF);
+		count++;
+	}
+	return (count);
 }
