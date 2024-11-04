00a014dc6b8d91a3eb30bd9f37e42ffdb16c9e53
TetsuroAndo <tetsuro997@gmail.com>
2024-11-04 22:03:17 +0900

    add files

---
+++ src/ft_stdio/ft_putchar.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 20:21:57 by teando            #+#    #+#             */
+/*   Updated: 2024/11/04 20:28:53 by teando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_putchar(int c)
+{
+	if (write(STDOUT_FILENO, &c, 1) == -1)
+		return (EOF);
+	return ((unsigned char)c);
+}
---
+++ src/ft_stdio/ft_putnbr.c
@@ -0,0 +1,42 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 21:23:32 by teando            #+#    #+#             */
+/*   Updated: 2024/11/04 21:34:44 by teando           ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "ft_stdio.h"
+#include <stddef.h>
+#include <stdio.h>
+
+int	ft_putnbr(int n)
+{
+	char			buffer[12];
+	unsigned int	num;
+	char			*c;
+  int count;
+
+  num = n ^ ((n >> 31) & ((unsigned int)(-n - n)));
+	c = &buffer[12];
+	*--c = '\0';
+	while (num || !*c)
+	{
+		*--c = num % 10 + '0';
+		num /= 10;
+	}
+	if (n < 0)
+		*--c = '-';
+  count = 0;
+  while (*c)
+  {
+    if(ft_putchar(*c++) == EOF)
+      return (EOF);
+    count++;
+  }
+  return (count);
+}
