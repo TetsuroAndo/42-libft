fab18dcb5f425b3220961d2e77da57515f8fb0d4
Tetsuro Ando <tetsuro997@gmail.com>
2024-11-17 22:25:52 +0900

    fixed realloc

--- ft_stdlib/ft_realloc.c
+++ ft_stdlib/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 03:51:06 by teando            #+#    #+#             */
-/*   Updated: 2024/11/08 04:08:57 by teando           ###   ########.fr       */
+/*   Updated: 2024/11/17 22:21:14 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,25 @@
 void	*ft_realloc(void *ptr, size_t size)
 {
 	void	*new_ptr;
+	size_t	old_size;
 
 	if (!ptr)
-		return (malloc(size));
-	new_ptr = NULL;
-	if (size)
 	{
 		new_ptr = malloc(size);
-		if (!new_ptr)
-			return (NULL);
-		ft_memcpy(new_ptr, ptr, size);
+		if (new_ptr)
+			ft_bzero(new_ptr, size);
+		return (new_ptr);
 	}
+	if(!size)
+		return (free(ptr), NULL);
+	old_size = ft_strlen(ptr);
+	new_ptr = malloc(size);
+	if (!new_ptr)
+		return (NULL);
+	if (old_size < size)
+		ft_memcpy(new_ptr, ptr, old_size);
+	else
+		ft_memcpy(new_ptr, ptr, size);
 	free(ptr);
 	return (new_ptr);
 }
