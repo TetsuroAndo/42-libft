d7e0c6ed55376f283daca6537627b72fd8d29d09
Tetsuro Ando <teando@c2r6s10.42tokyo.jp>
2024-12-23 14:48:26 +0900

    fix strspn strcspn functions

--- ft_string/ft_strcspn.c
+++ ft_string/ft_strcspn.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 05:27:16 by teando            #+#    #+#             */
-/*   Updated: 2024/12/06 17:20:19 by teando           ###   ########.fr       */
+/*   Updated: 2024/12/23 14:47:03 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strcspn(const char *s, const char *reject)
 	size_t	i;
 
 	i = 0;
-	while (ft_strchr(reject, s[i]))
+	while (s[i] && !ft_strchr(reject, s[i]))
 		++i;
 	return (i);
 }
--- ft_string/ft_strspn.c
+++ ft_string/ft_strspn.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 05:27:14 by teando            #+#    #+#             */
-/*   Updated: 2024/12/06 16:13:41 by teando           ###   ########.fr       */
+/*   Updated: 2024/12/23 14:47:30 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ size_t	ft_strspn(const char *s, const char *accept)
 	size_t	i;
 
 	i = 0;
-	while (!ft_strchr(accept, s[i]))
+	while (s[i] && ft_strchr(accept, s[i]))
 		++i;
 	return (i);
 }
