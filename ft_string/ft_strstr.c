39f950d23e3af0ac5e8df8f26f6a6e5c61f0a585
Tetsuro Ando <tetsuro997@gmail.com>
2025-03-26 07:01:05 +0900

    file Split.

--- ft_string/ft_strstr.c
+++ ft_string/ft_strstr.c
@@ -1,32 +1,16 @@
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/21 02:22:52 by teando            #+#    #+#             */
-/*   Updated: 2025/03/26 06:59:22 by teando           ###   ########.fr       */
+/*   Created: 2025/03/26 06:59:24 by teando            #+#    #+#             */
+/*   Updated: 2025/03/26 06:59:24 by teando           ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-#include "ft_string.h"
-#include <stddef.h>
-
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strstr(const char *big, const char *little)
 {
-	size_t	little_len;
-
-	if (!*little)
-		return ((char *)big);
-	if (!len || !*big)
-		return (NULL);
-	little_len = ft_strlen(little);
-	while (little_len <= len--)
-	{
-		if (*big == *little && !ft_strncmp(big, little, little_len))
-			return ((char *)big);
-		big++;
-	}
-	return (NULL);
+	return (ft_strnstr(big, little, ft_strlen(big)));
 }
