ef19729d2d6118e57890e3c460b8c9e0ef5bbe6e
Tetsuro Ando <tetsuro997@gmail.com>
2025-03-26 07:04:52 +0900

    file Split.

--- incs/ft_stdio.h
+++ incs/ft_stdio.h
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_stdio.h                                         :+:      :+:    :+:   */
@@ -6,9 +6,9 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 02:17:34 by teando            #+#    #+#             */
-/*   Updated: 2024/12/17 23:41:03 by teando           ###   ########.fr       */
+/*   Updated: 2025/03/26 07:04:24 by teando           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef FT_STDIO_H
 # define FT_STDIO_H
@@ -16,12 +16,12 @@
 # include "ft_printf.h"
 # include <unistd.h>
 
-int		ft_putchar_fd(char c, int fd);
-int		ft_putchar(int c);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-int		ft_putstr_fd(char *s, int fd);
-int		ft_putstrs_fd(char **strs, char *delimiter, int fd);
-int		ft_putstrs_endl_fd(char **strs, char *delimiter, int fd);
+int	ft_putchar_fd(char c, int fd);
+int	ft_putchar(int c);
+int	ft_putendl_fd(char *s, int fd);
+int	ft_putnbr_fd(int n, int fd);
+int	ft_putstr_fd(char *s, int fd);
+int	ft_putstrs_fd(char **strs, char *delimiter, int fd);
+int	ft_putstrs_endl_fd(char **strs, char *delimiter, int fd);
 
 #endif
